067c5897645ff496eb50718721d87a26fa3bc78e
Ashung PC <ashung.hung@gmail.com>
2015-01-09 22:26:26 +0800

    First

--- main2.js
+++ main2.js
@@ -2,7 +2,7 @@ var xml = require("node-xml-lite");
 var svg = xml.parseFileSync('weather_cloudy_ps.svg');
 
 
-//console.log(JSON.stringify(xml.parseFileSync('ic_visibility_48px.svg'), '', '  '));
+console.log(JSON.stringify(xml.parseFileSync('weather_cloudy_ps.svg'), '', '  '));
 
 
 
@@ -39,14 +39,39 @@ function travel(obj, levelIndex) {
             travel(obj[i].childs, levelIndex);
             vectorDrawableXML += repeatString(' ', levelIndex * 4) + '</group>\n';
         } else if(obj[i].name == 'path') {
-            vectorDrawableXML += repeatString(' ', levelIndex * 4) + '<path android:pathData="' + obj[i].attrib.d + '"/>\n';
+            vectorDrawableXML += repeatString(' ', levelIndex * 4) + '<path\n';
+            
+            // Sketch SVG files use shapes name for id attribute.
+            if(hasArrtib(obj[i].attrib, 'id')){
+                vectorDrawableXML += repeatString(' ', levelIndex * 5) + 'android:name="' + obj[i].attrib.id + '"\n';
+            }
+            vectorDrawableXML += repeatString(' ', levelIndex * 5) + 'android:fillColor="' + getArrtibValue(obj[i].attrib, 'fill') + '"\n'
+            
+            
+            
+            
+            vectorDrawableXML += repeatString(' ', levelIndex * 5) + 'android:pathData="' + obj[i].attrib.d + '"/>\n';
+            
+            
             //console.log();
+            /*
+            for(v in obj[i].attrib) {
+                console.log(v);
+            }
+            */
+            
+            
+            
         }
         
+        
+        
+        
     }
-    
-    
 }
+
+
+
 
 travel(svg.childs, 0);
 
@@ -57,14 +82,69 @@ travel(svg.childs, 0);
 console.log(vectorDrawableXML)
 
 
+console.log(getStyleBlock(svg.childs));
+
+
+
 function repeatString(str, num) {
     var t = '';
     for(var i = 0; i < num; i ++) {
         t += ' ';
     }
     return t;
-    
 }
+
+function hasArrtib(obj, attrib) {
+    return attrib in obj;
+}
+
+function getStyleBlock(obj) {
+    /*
+    for(var i = 0; i < obj.length; i ++) {
+        console.log(JSON.stringify(obj[i]) + '\n-------------------------');
+    }
+    */
+    
+    t(obj);
+    
+    
+    function t(o) {
+       
+        for(var i = 0; i < o.length; i ++) {
+            console.log(i + '-------------------------' + (o[i].name));
+            /*
+            if('childs' in o[i]) {
+                t(o[i]);
+            }
+            if(o[i].name == 'style') {
+                return o[i].childs[0];
+            }
+            */
+        }
+        
+        
+        //return JSON.stringify(o);
+        
+    }
+    
+    
+    
+    
+    //return JSON.stringify(obj);
+}
+
+function getArrtibValue(obj, attrib) {
+    if(hasArrtib(obj, attrib))
+        return eval('obj.' + attrib);
+    
+    if(hasArrtib(obj, 'style')) {
+        var style = obj.style;
+    }
+    
+    
+    return "#000";
+}
+
 
 
 
@@ -77,8 +157,8 @@ path = path
     fill-opacity = fillAlpha
     stroke = strokeColor
     stroke-opacity = strokeAlpha
-    stroke-width = strokeWidth            
-    stroke-linejoin = strokeLineJoin          
+    stroke-width = strokeWidth
+    stroke-linejoin = strokeLineJoin
     stroke-miterlimit = strokeMiterLimit
     stroke-linecap = strokeLineCap
 */
